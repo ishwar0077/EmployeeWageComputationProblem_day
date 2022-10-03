ccc14867be61d454dd37147a4f038063550274c2
Ishwar Rathod <ishwarkrathod.o7@gmail.com>
2022-10-03 14:16:02 +0530

    Greeting Message

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿
+namespace EmplyeeWageProblem
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Program");
+    
+        }
+    }
+
+}
