5c8243972e080afde619483dcb4ceda36c1ac94a
Ishwar Rathod <ishwarkrathod.o7@gmail.com>
2022-10-04 17:07:34 +0530

    Using Switch case

--- EmployeeCheck.cs
+++ EmployeeCheck.cs
@@ -10,30 +10,32 @@ namespace EmployeeWageProblem
     {
         public static void EmployeeAttendance()
         {
-            int Is_Full_time = 1;
-            int Is_Part_time = 2;
-            int Emp_Rate_per_hour = 20;
+           const int Is_Full_time = 1;
+           const int Is_Part_time = 2;
+           const int Emp_Rate_per_hour = 20;
             //Variables
             int empHrs = 0;
             int empWage = 0;
             Random rand = new Random();
             int empCheck = rand.Next(3);
-            //Computation
-            if(empCheck == Is_Full_time)
-            {
-                empHrs = 8;
-            }
-            else if(empCheck == Is_Part_time)
-            {
-               empHrs = 4;
-            }
-            else
-            { 
-                empHrs = 0;
-            }
-            empWage = Emp_Rate_per_hour * empHrs;
-            Console.WriteLine("Employee Wage" + empWage);
 
+                //Computation
+                switch (empCheck)
+                {
+                    case Is_Full_time:
+                        empHrs = 8;
+                        break;
+
+                    case Is_Part_time:
+                        empHrs = 4;
+                        break;
+
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+            empWage = Emp_Rate_per_hour * empHrs;
+            Console.WriteLine("total employee wage is: " + empWage);
         }
     }
 }
